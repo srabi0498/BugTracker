f7a01bd6d310373bf29e137e85c30d8b268103ed
Rabi kumar sah <165571303+srabi0498@users.noreply.github.com>
2024-06-11 18:49:08 +0545

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BugTracker.Models;
+namespace BugTracker.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+                        return View();
+        }
+    }
+}
